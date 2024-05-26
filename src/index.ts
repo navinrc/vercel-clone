cb6e0e7f8d03301a4e76cd999757bbb6245cffc9
navinrc <navinrc98@gmail.com>
2024-05-26 20:19:55 +0530

    ipload files to s3 logic using presigned url

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
-import { generate } from "./utils";
+import { generate, getAllFiles, uploadFile } from "./utils";
+import path from "path";
+import "dotenv/config"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/deploy", async (req, res) => {
+app.put("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl; //github.com/navinrc/react-boilerplate-code
   const id = generate();
-  await simpleGit().clone(repoUrl, `output/${id}`);
-  console.log("repourl", repoUrl);
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+  //console.log("repourl", repoUrl);   
+
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
+  console.log(files)
+  files.forEach(async file => {
+    await uploadFile(file.slice(__dirname.length + 1), file);
+})
   res.json({
-    id
+    id,
   });
 });
 app.listen(3000);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+import fs from "fs";
+import path from "path";
+const AWS = require("aws-sdk");
+const axios = require("axios");
+
 const MAX_LEN = 5;
 
 export const generate = () => {
@@ -7,4 +12,58 @@ export const generate = () => {
     ans += source[Math.floor(Math.random() * source.length)];
   }
   return ans;
+};
+
+export const getAllFiles = (folderPath: string) => {
+  let response: string[] = [];
+
+  const allFilesAndFolders = fs.readdirSync(folderPath);
+  allFilesAndFolders.forEach((file) => {
+    const fullFilePath = path.join(folderPath, file);
+    if (fs.statSync(fullFilePath).isDirectory()) {
+      response = response.concat(getAllFiles(fullFilePath));
+    } else {
+      response.push(fullFilePath);
+    }
+  });
+  return response;
+};
+
+// Configure AWS SDK
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+  // console.log("inside upload file service", fileName, process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
+  const fileContent = fs.readFileSync(localFilePath);
+
+  // Set the parameters for the presigned URL
+  const params = {
+    Bucket: "vercel-navinrc",
+    Key: fileName, // The key (name) of the object in the bucket
+    ContentType: "application/octet-stream", // Content type of the object
+    Expires: 3600, // The URL will expire in 3600 seconds (1 hour)
+  };
+
+  // Generate the presigned URL
+  const url = s3.getSignedUrl("putObject", params);
+
+  // console.log("Presigned URL:", url);
+
+  // Make a PUT request to the presigned URL
+  axios
+    .put(url, fileContent, {
+      headers: {
+        "Content-Type": "application/octet-stream", // Set the content type of the file
+      },
+    })
+    .then((_response: any) => {
+      console.info("File uploaded successfully", fileName);
+    })
+    .catch((error: any) => {
+      console.error("Error uploading file:", fileName, error);
+    });
 };
